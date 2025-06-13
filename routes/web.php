56b3d32e42aa0ccc223aa818bca9433b5a660077
Marcos Bastos <167112008+Mariogi40@users.noreply.github.com>
2025-06-13 13:04:24 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,13 @@
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/piscar', function () {
-    return view('piscar');
+Route::get('/', function () {
+    return view('index');
 });
 
 Route::get('/vendas', function () {
     return view('vendas'); // vai buscar resources/views/vendas.blade.php
 })->name('vendas');
-
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
